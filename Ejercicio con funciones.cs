9c9019c63903826ed843652a6b4f858d72aebaed
Kvssa250 <122424551+Kvssa250@users.noreply.github.com>
2023-01-19 19:08:29 -0400

    Add files via upload

---
+++ Ejercicio con funciones.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Security.Cryptography;
+
+namespace Ejercicio_con_funciones_pero_de_verdad
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el año que desee evaluar: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            int prueba1 = year % 4;
+            int prueba2 = year % 100;
+            int prueba3 = year % 400;
+
+            if (prueba1 == 0)
+            {
+                if (prueba2 != 0)
+                {
+                    Verdadero();
+                }
+                else if (prueba2 == 0)
+                {
+                    if (prueba3 == 0)
+                    {
+                        Verdadero();
+                    }
+                    else if (prueba3 != 0)
+                    {
+                        Falso();
+                    }
+                }
+            }
+            else if (prueba1 != 0)
+            {
+                Falso();
+            }
+        }
+        public static void Verdadero()
+        {
+            Console.WriteLine("El año que ingreso si es bisiesto!");
+        }
+        public static void Falso()
+        {
+            Console.WriteLine("El año que ingreso no es bisiesto.");
+        }
+    }
+}
